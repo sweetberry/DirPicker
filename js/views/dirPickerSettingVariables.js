07949156f3003680334f875d66f5bfaf7ebc5663
sweetberry <pixel@sweetberry.com>
2016-02-07 22:12:43 +0900

    Change reRender timing
    reorderOnSortを使ってコレクション変更時の再描画を抑制。

--- js/views/dirPickerSettingVariables.js
+++ js/views/dirPickerSettingVariables.js
@@ -1,29 +1,26 @@
 var templateHtml = require( '../templates/dirPickerSettingVariables.html' );
-var ChildView = require( './dirPickerSettingVariable' );
+var DirPickerSettingVariableView = require( './dirPickerSettingVariable' );
+var DirPickerVariablesCollection = require( '../collections/dirPickerVariables' );
 
 //noinspection JSUnusedGlobalSymbols
 var ViewsDirPickerSettingVariables = Backbone.Marionette.CompositeView.extend( {
-  collection: require( '../collections/dirPickerVariables' ),//variablesCollection
-  childView: ChildView,
-  template: templateHtml,
-  className: "",
+  collection        : DirPickerVariablesCollection,
+  childView         : DirPickerSettingVariableView,
+  template          : templateHtml,
+  className         : "",
   childViewContainer: '.js-variable-list-container',
-  collectionEvents: {
-    'sort': 'render',
-    'add': 'render',
-    'remove': 'render'
-  },
-  ui: {
+  reorderOnSort     : true,
+  ui                : {
     childViewContainer: '.js-variable-list-container',
-    addBtn: '.js-variable-add-btn',
-    handle: '.js-variable-handle'
+    addBtn            : '.js-variable-add-btn',
+    handle            : '.js-variable-handle'
   },
-  events: {
-    'click @ui.addBtn': 'onAddClick',
+  events            : {
+    'click @ui.addBtn'     : 'onClickAddBtn',
     'mouseenter @ui.handle': 'sortStart',
     'mouseleave @ui.handle': 'sortDestroy'
   },
-  templateHelpers: function () {
+  templateHelpers   : function () {
     var _self = this;
     //noinspection JSUnusedGlobalSymbols
     return {
@@ -33,29 +30,32 @@ var ViewsDirPickerSettingVariables = Backbone.Marionette.CompositeView.extend( {
     }
   },
 
-  sortStart: function () {
+  sortStart      : function () {
     var _self = this;
     this.ui.childViewContainer.sortable( {
       forcePlaceholderSize: true,
       //placeholderClass: 'col-lg-4 col-md-6',
-      handle: '.js-variable-handle'
+      handle              : '.js-variable-handle'
     } ).bind( 'sortupdate', function () {
       //console.log("variablesSort");
       _self.updateItemSortIndex( _self.ui.childViewContainer.find( '.js-variable-model-id' ) );
     } );
   },
-  sortDestroy: function () {
-    this.ui.childViewContainer.sortable( 'destroy' );
+  sortDestroy    : function () {
+    if (this.ui.childViewContainer.sortable) {
+      this.ui.childViewContainer.sortable( 'destroy' );
+    }
   },
-  initialize: function () {
+  initialize     : function () {
     //this.debugEvents('VariablesView');
   },
-  onRender: function () {
+  onRender       : function () {
+    //console.log( 'variablesRender' );
   },
   onBeforeDestroy: function () {
-    //this.ui.childViewContainer.sortable( 'destroy' );
+    this.sortDestroy();
   },
-  onAddClick: function () {
+  onClickAddBtn     : function () {
     this.collection.create( {}, {wait: true} );
   },
 
@@ -64,12 +64,12 @@ var ViewsDirPickerSettingVariables = Backbone.Marionette.CompositeView.extend( {
    * @param elements data-model-idの指定があるエレメントの配列
    */
   updateItemSortIndex: function ( elements ) {
+    console.log( 'updateItemSortIndex' );
     var _self = this;
     _.each( elements, function ( element, index ) {
       //noinspection JSUnresolvedVariable
       _self.collection.get( element.dataset.modelId ).save( 'sort', index );
     } );
-    this.collection.sort();
   }
 
 } );
