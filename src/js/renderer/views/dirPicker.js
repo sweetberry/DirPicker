3d1c715d8cac1008e20532716e63e14250e1dc68
sweetberry <pixel@sweetberry.com>
2016-02-13 22:58:34 +0900

    Refactor to ES6

--- src/js/renderer/views/dirPicker.js
+++ src/js/renderer/views/dirPicker.js
@@ -1,20 +1,19 @@
-var template = require( '../templates/dirPicker.html' );
-var _ = require( 'underscore' );
-var open = require( 'open' );
-var path = require( 'path' );
+"use strict";
 
-//noinspection JSUnusedGlobalSymbols
-module.exports = Backbone.Marionette.LayoutView.extend( {
-  className      : "js-view-dir-picker",
-  template       : template,
-  /**
-   * @type {ModelsDirPickerAppState}
-   */
-  model          : require( '../models/dirPickerAppState' ),
-  modelEvents    : {
+import DIR_PICKER_TEMPLATE from '../templates/dirPicker.html';
+import _ from 'underscore';
+import path from 'path';
+import dirPickerAppStateModel from '../models/dirPickerAppState';
+import {LayoutView} from 'backbone.marionette';
+
+export default class DirPickerView extends LayoutView.extend( {
+  className  : "js-view-dir-picker",
+  template   : DIR_PICKER_TEMPLATE,
+  model      : dirPickerAppStateModel,
+  modelEvents: {
     'change': 'render'
   },
-  ui             : {
+  ui         : {
     selectTemplate: '#selectTemplate',
     resultPanel   : '.js-result-panel',
     variableInput : '.js-variable-input',
@@ -24,47 +23,47 @@ module.exports = Backbone.Marionette.LayoutView.extend( {
     clipBtn       : '.js-clip-btn',
     pathSeg       : '.js-result-path-seg'
   },
-  events         : {
+  events     : {
     'change @ui.selectTemplate': 'onChangeTemplate',
-    //'change @ui.variableSelect': 'onChangeSelect',
     'change @ui.variableInput' : 'onChangeVariable',
     'keyup @ui.variableInput'  : 'onKeyUpVariable',
     'click @ui.openBtn'        : 'onClickOpen',
     'click @ui.createBtn'      : 'onClickCreate',
     'click @ui.clipBtn'        : 'onClickClip',
     'dblclick @ui.pathSeg'     : 'onClickPathSeg'
-  },
-  templateHelpers: function () {
-    var _self = this;
+  }
+} ) {
+
+  templateHelpers () {
     //noinspection JSUnusedGlobalSymbols
     return {
-      getTemplates        : function () {
-        return _self.model.getTemplates();
+      getTemplates        : ()=> {
+        return this.model.getTemplates();
       },
-      getUsedVariablesList: function () {
-        return _self.model.getUsedVariablesList();
+      getUsedVariablesList: ()=> {
+        return this.model.getUsedVariablesList();
       },
-      getEvaluatedPath    : function () {
-        return _self.model.getEvaluatedPath();
+      getEvaluatedPath    : ()=> {
+        return this.model.getEvaluatedPath();
       },
-      getSubDirLinkedPath : function () {
-        var pathString = _self.model.getEvaluatedPath().path;
-        var sepPathArray = pathString.split( path.sep );
-        var resPathArray = [];
+      getSubDirLinkedPath : ()=> {
+        const pathString = this.model.getEvaluatedPath().path;
+        const sepPathArray = pathString.split( path.sep );
+        const resPathArray = [];
         while (sepPathArray.length) {
           resPathArray.unshift( sepPathArray.join( path.sep ) );
           sepPathArray.pop();
         }
-        return _.map( _.zip( pathString.split( path.sep ), resPathArray ), function ( raw ) {
-          return '<span class="js-result-path-seg" data-path="' + raw[1] + '">' + _.escape(raw[0]) + '</span>'
+        return _.map( _.zip( pathString.split( path.sep ), resPathArray ), ( raw )=> {
+          return `<span class="js-result-path-seg" data-path="${raw[1]}">${_.escape( raw[0] )}</span>`;
         } ).join( path.sep );
 
       }
     }
-  },
 
-  onRender: function () {
+  }
 
+  onRender () {
     this.model.save();
 
     //templateのautoComplete
@@ -79,49 +78,42 @@ module.exports = Backbone.Marionette.LayoutView.extend( {
     } );
 
     //VariableのautoComplete
-    var usedVariablesList = this.model.getUsedVariablesList();
-    var _self = this;
-    _.each( this.ui.variableSelect, function ( element ) {
-      if ($( element ).typeahead) {
-        $( element ).typeahead( 'destroy' );
-      }
+    _.each( this.ui.variableSelect, ( element )=> {
+      const $el = $( element );
+
+      if ($el.typeahead) {$el.typeahead( 'destroy' )}
       //js-variable-select
-      var targetUid = $( element ).attr( "id" );
-      var targetListRow = _.find( usedVariablesList, {uid: targetUid} );
-      $( element ).typeahead( {
+      const targetListRow = _.find( this.model.getUsedVariablesList(), {uid: $el.attr( "id" )} );
+      $el.typeahead( {
         source         : targetListRow.list,
         items          : 'all',
         showHintOnFocus: true,
         minLength      : 0,
-        displayText    : function ( item ) {return item.label},
-        afterSelect    : function ( item ) {
+        displayText    : ( item )=> {return item.label},
+        afterSelect    : ( item )=> {
           element.dataset.variableValue = item.val;
-          _self.setValues();
-          _self.render();
+          this.setValues();
+          this.render();
         }
       } );
 
     } );
-  },
+  }
 
-  onChangeTemplate: function () {
+  onChangeTemplate () {
     this.model.set( {'template': this.ui.selectTemplate.val()}, {validate: true} );
-  },
-  onChangeSelect  : function () {
-    //console.log( 'onChangeSelect' );
-    this.render();
-  },
+  }
 
-  onChangeVariable: function ( e ) {
-    var element = e.target;
+  onChangeVariable ( e ) {
+    const element = e.target;
     element.dataset.variableValue = element.value;
     this.setValues();
     this.render();
-  },
+  }
 
-  onKeyUpVariable: function ( e ) {
-    var difference = 0;
-    var element = e.target;
+  onKeyUpVariable ( e ) {
+    let difference = 0;
+    const element = e.target;
 
     if (e.keyCode == 38) {
       difference = 1;
@@ -132,44 +124,48 @@ module.exports = Backbone.Marionette.LayoutView.extend( {
     } else {
       return;
     }
-    var targetVal = element.value;
+
+    const targetVal = element.value;
     //console.log( "targetVal:", targetVal );
-    var lastPaddingRegexp = /\d+$/;
-    var lastPadding = lastPaddingRegexp.exec( targetVal );
+    const LAST_PADDING_REGEXP = /\d+$/;
+    const lastPadding = LAST_PADDING_REGEXP.exec( targetVal );
     //console.log( "lastPadding:", lastPadding );
     if (lastPadding) {
-      var dstPadding = parseInt( lastPadding ) + difference;
+
+      //TODO:ここで桁数が変わってしまっています!
+      const dstPadding = parseInt( lastPadding ) + difference;
       //console.log( "dst:", targetVal.replace( lastPaddingRegexp, dstPadding ) );
       if (dstPadding >= 0) {
-        element.dataset.variableValue = targetVal.replace( lastPaddingRegexp, dstPadding );
+        element.dataset.variableValue = targetVal.replace( LAST_PADDING_REGEXP, dstPadding );
         this.setValues();
-        $( element ).val( targetVal.replace( lastPaddingRegexp, dstPadding ) )
+        $( element ).val( targetVal.replace( LAST_PADDING_REGEXP, dstPadding ) );
       }
     }
-  },
-
-  onClickPathSeg: function ( e ) {
-    open( e.target.dataset.path );
-  },
-
-  onClickOpen  : function () {
-    this.model.openPath();
-  },
-  onClickCreate: function () {
-    this.model.createPath();
-    this.render();
-  },
-  onClickClip  : function () {
-    this.model.clipPath();
-  },
-  setValues    : function () {
-    //console.log( 'setValues' );
-    var _self = this;
-    var targetElement = this.ui.variableSelect.add( this.ui.variableInput );
-    _.each( targetElement, function ( element ) {
-      //noinspection JSUnresolvedVariable
-      _self.model.setValue( element.dataset.variableName, element.dataset.variableValue );
-    } );
   }
 
-} );
+  onClickPathSeg ( e ) {
+    this.model.openPath( e.target.dataset.path );
+  }
+
+  onClickOpen () {
+    this.model.openPath();
+  }
+
+  onClickCreate () {
+    this.model.createPath();
+    this.render();
+  }
+
+  onClickClip () {
+    this.model.clipPath();
+  }
+
+  setValues () {
+    //console.log( 'setValues' );
+    const targetElement = this.ui.variableSelect.add( this.ui.variableInput );
+    _.each( targetElement, ( element )=> {
+      //noinspection JSUnresolvedVariable
+      this.model.setValue( element.dataset.variableName, element.dataset.variableValue );
+    } );
+  }
+}
