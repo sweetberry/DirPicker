6806b435bb2721eb15d4ed468b79904d3671d7a5
sweetberry <pixel@sweetberry.com>
2016-02-13 15:46:08 +0900

    Refactor to es6
    es6で書き直し

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,46 @@
 "use strict";
-const BrowserWindow = require( 'browser-window' );  // Module to create native browser window.
 
-// Report crashes to our server.
-require( 'crash-reporter' ).start();
+import path from 'path';
+import electron from 'electron';
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const dialog = electron.dialog;
+const ipcMain = electron.ipcMain;
+const presetSettingJsonPath = path.join( app.getPath( 'desktop' ), "dirPickerSetting.json" );
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is GCed.
 let mainWindow = null;
 
-const app = require( 'app' );  // Module to control application life.
-const dialog = require( 'dialog' );
-const ipcMain = require( 'electron' ).ipcMain;
-
-
-// エラー表示後終了
-ipcMain.on( 'error-message', function ( event, arg ) {
+ipcMain.on( 'error-message', ( event, arg )=> {
   dialog.showErrorBox( "Error", arg );
   event.returnValue = true;
 } );
 
-const presetSettingJsonPath = require( "path" ).join( app.getPath( 'desktop' ), "dirPickerSetting.json" );
-ipcMain.on( 'get-setting-file-save-path', function ( event, defaultPath ) {
+ipcMain.on( 'get-setting-file-save-path', ( event, defaultPath )=> {
   dialog.showSaveDialog( {
     title      : "設定ファイルの保存先を指定してください",
     defaultPath: defaultPath || presetSettingJsonPath
-  }, function ( res ) {
+  }, ( res )=> {
     event.returnValue = res || false;
   } );
 } );
 
-ipcMain.on( 'get-setting-file-load-path', function ( event, defaultPath ) {
+ipcMain.on( 'get-setting-file-load-path', ( event, defaultPath )=> {
   dialog.showOpenDialog( {
     title      : "設定ファイルを選択してください",
     defaultPath: defaultPath || presetSettingJsonPath,
     filters    : [
       {name: 'Custom File Type', extensions: ['json']}
     ]
-  }, function ( res ) {
+  }, ( res )=> {
     event.returnValue = res || false;
   } );
 } );
 
-// Quit when all windows are closed.
-app.on( 'window-all-closed', function () {
+app.on( 'window-all-closed', ()=> {
   app.quit();
 } );
 
-// This method will be called when atom-shell has done everything
-// initialization and ready for creating browser windows.
-app.on( 'ready', function () {
+app.on( 'ready', ()=> {
 
   // Create the browser window.
   mainWindow = new BrowserWindow( {
@@ -60,7 +52,7 @@ app.on( 'ready', function () {
   mainWindow.loadUrl( 'file://' + __dirname + '/../html/renderer/index.html' );
 
   // Emitted when the window is closed.
-  mainWindow.on( 'closed', function () {
+  mainWindow.on( 'closed', ()=> {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
