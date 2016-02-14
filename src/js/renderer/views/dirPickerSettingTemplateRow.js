f098fef0adf8417d5b27a058df04028d00f803a3
sweetberry <pixel@sweetberry.com>
2016-02-14 22:53:29 +0900

    Refactor anotation

--- src/js/renderer/views/dirPickerSettingTemplateRow.js
+++ src/js/renderer/views/dirPickerSettingTemplateRow.js
@@ -32,6 +32,10 @@ export default class DirPickerSettingTemplateRowView extends LayoutView.extend( 
 } ) {
 
   //noinspection JSUnusedGlobalSymbols
+  /**
+   *
+   * @returns {{getColoredTemplatePath: getColoredTemplatePath}}
+   */
   templateHelpers () {
     const ESCAPED_TAG_REGEXP = /(&lt;.*?&gt;)/g;
     //noinspection JSUnusedGlobalSymbols
@@ -56,45 +60,73 @@ export default class DirPickerSettingTemplateRowView extends LayoutView.extend( 
   }
 
   //noinspection JSUnusedGlobalSymbols
+  /**
+   *
+   */
   onRender () {
     this.ui.handle.tooltip( 'destroy' );
     this.ui.handle.tooltip();
   }
 
   //noinspection JSUnusedGlobalSymbols
+  /**
+   *
+   */
   onBeforeDestroy () {
     this.ui.handle.tooltip( 'destroy' );
   }
 
+  /**
+   * enterの入力を監視します
+   * @param {event} e
+   */
   onKeyupNameInput ( e ) {
     if (e.which == 13) {this.changeNameInput();}
   }
 
+  /**
+   * enterの入力を監視します
+   */
   onKeyupPathInput () {
     if (e.which == 13) {this.changePathInput();}
   }
 
+  /**
+   * inputに表示を切り替える
+   */
   onClickName () {
     this.ui.name.addClass( 'hide' );
     this.ui.nameInput.removeClass( 'hide' );
     this.ui.nameInput.focus();
   }
 
+  /**
+   * inputに表示を切り替える
+   */
   onClickPath () {
     this.ui.path.addClass( 'hide' );
     this.ui.pathInput.removeClass( 'hide' );
     this.ui.pathInput.focus();
   }
 
+  /**
+   * rowを削除します。
+   */
   onClickDelBtn () {
     this.model.destroy();
   }
 
+  /**
+   * inputの中身をデータに反映して再描画
+   */
   changeNameInput () {
     this.model.save( 'name', this.ui.nameInput.val(), {silent: false, wait: true} );
     this.render();
   }
 
+  /**
+   * inputの中身をデータに反映して再描画
+   */
   changePathInput () {
     this.model.save( 'path', this.ui.pathInput.val(), {silent: false, wait: true} );
     this.render();
