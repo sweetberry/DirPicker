1a532850bacfb85ad2b62c313a0582b03582b30e
sweetberry <pixel@sweetberry.com>
2016-02-14 14:54:00 +0900

    Refactor to ES6

--- src/js/renderer/collections/dirPickerVariableList.js
+++ src/js/renderer/collections/dirPickerVariableList.js
@@ -1,24 +1,40 @@
 "use strict";
 
-const _ = require( 'underscore' );
-const Backbone = require( 'backbone' );
-Backbone.LocalStorage = require( "backbone.localstorage" );
-//require( 'backbone-event-logger' );
+import DirPickerCollectionBase from './dirPickerCollectionBase';
+import VariableRowModel from '../models/dirPickerVariableRow'
 
-const VariableRowModel = require( '../models/dirPickerVariableRow' );
+export default class DirPickerVariableList extends DirPickerCollectionBase {
 
-//noinspection JSUnusedGlobalSymbols
-/**
- * @class
- * @extends {Backbone.Collection}
- */
-const CollectionsDirPickerVariableList = Backbone.Collection.extend( {
-  initialize: function () {
-    //this.debugEvents('CollectionsVariableList');
-  },
-  model     : VariableRowModel,
-  comparator: 'sort'
-} );
-_.extend( CollectionsDirPickerVariableList.prototype, require( './mixin' ) );
+  /**
+   * @param {object} [attr]
+   * @param {object} [options]
+   */
+  constructor ( attr, options ) {
+    super( attr, options );
 
-module.exports = CollectionsDirPickerVariableList;
+    /**
+     *
+     * @type {VariableRowModel}
+     */
+    this.model = VariableRowModel;
+
+    //noinspection JSUnusedGlobalSymbols
+    /**
+     *
+     * @type {string}
+     */
+    this.comparator = 'sort'
+
+  }
+
+  //noinspection JSUnusedGlobalSymbols,JSUnusedLocalSymbols,JSUnusedLocalSymbols
+  /**
+   *
+   * @param {object} [attr]
+   * @param {object} [options]
+   */
+  initialize ( attr, options ) {
+    this.debugEvents( 'CollectionsVariableList' );
+  }
+
+}
