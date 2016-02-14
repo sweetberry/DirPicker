8f08fa3d772266e5b6b74af9d6e1a4b8fc203ba6
sweetberry <pixel@sweetberry.com>
2016-02-14 23:33:54 +0900

    Refactor to ES6

--- src/js/renderer/views/dirPickerSettingVariable.js
+++ src/js/renderer/views/dirPickerSettingVariable.js
@@ -1,11 +1,18 @@
-var templateHtml = require( '../templates/dirPickerSettingVariable.html' );
-var DirPickerSettingVariableRowView = require( './dirPickerSettingVariableRow' );
+"use strict";
 
-//noinspection JSUnusedGlobalSymbols
-var ViewsDirPickerSettingVariable = Backbone.Marionette.CompositeView.extend( {
+import _ from 'underscore';
+import DIR_PICKER_SETTING_VARIABLE_TEMPLATE from '../templates/dirPickerSettingVariable.html';
+import DirPickerSettingVariableRowView from  './dirPickerSettingVariableRow';
+import {CompositeView} from 'backbone.marionette';
+// import 'backbone-event-logger';
+
+/**
+ * setting画面のvariableを扱うviewです。
+ */
+export default class DirPickerSettingVariableView extends CompositeView.extend( {
   model             : undefined,//variableModel
   collection        : undefined,//variableModel.listCollection
-  template          : templateHtml,
+  template          : DIR_PICKER_SETTING_VARIABLE_TEMPLATE,
   childView         : DirPickerSettingVariableRowView,
   childViewContainer: '.js-variable-row-list-container',
   className         : 'col-lg-4 col-md-6',
@@ -36,85 +43,137 @@ var ViewsDirPickerSettingVariable = Backbone.Marionette.CompositeView.extend( {
   modelEvents       : {
     'change:name': 'redrawName',
     'badgeUpdate': 'badgeUpdate'
-  },
+  }
+} ) {
 
   /**
-   * renderし直したくないんで
+   * @param {object} [options]
    */
-  badgeUpdate        : function () {
-    this.ui.badge.text( this.collection.length );
-  },
-  onClickCloseBtn    : function () {
-    this.ui.collapse.collapse( 'toggle' );
-  },
-  initialize         : function () {
-    //this.debugEvents('VariableView');
-    this.collection = this.model.listCollection
-  },
-  sortStart          : function () {
-    var _self = this;
-    this.ui.childViewContainer.sortable( {
-      forcePlaceholderSize: true,
-      handle              : '.js-variable-row-handle'
-    } ).bind( 'sortupdate', function () {
-      //console.log("variablesSort");
-      _self.updateItemSortIndex( _self.ui.childViewContainer.find( '.js-variable-row-model-id' ) );
-    } );
-  },
-  sortDestroy        : function () {
-    if (this.ui.childViewContainer.sortable) {
-      this.ui.childViewContainer.sortable( 'destroy' );
-    }
-  },
-  onRender           : function () {
+  constructor ( options ) {
+    super( options );
+    // this.debugEvents('VariableView');
+
+    /**
+     * リスト型変数の項目リストCollection
+     * @type {DirPickerVariableList}
+     */
+    this.collection = this.model.listCollection;
+  }
+
+  /**
+   *
+   */
+  onRender () {
     this.ui.handle.tooltip();
-  },
-  onBeforeDestroy    : function () {
+  }
+
+  //noinspection JSUnusedGlobalSymbols
+  /**
+   *
+   */
+  onBeforeDestroy () {
     this.ui.handle.tooltip( 'destroy' );
-    this.sortDestroy()
-  },
-  onKeyupNameInput   : function ( e ) {
-    if (e.which == 13) {
-      this.ui.nameInput.blur();
-    }
-  },
-  onClickName        : function () {
+    this.sortDestroy();
+  }
+
+  /**
+   * inputに表示を切り替える
+   */
+  onClickName () {
     this.ui.name.addClass( 'hide' );
     this.ui.nameInput.removeClass( 'hide' );
     this.ui.nameInput.focus();
-  },
-  redrawName         : function () {
+  }
+
+  /**
+   * 新規変数を追加します。
+   */
+  onClickAddBtn () {
+    this.collection.add( {}, {wait: true} );
+    this.ui.collapse.collapse( 'show' );
+    this.onRender();
+  }
+
+  /**
+   * 変数を削除します
+   */
+  onClickDelBtn () {
+    this.model.destroy();
+  }
+
+  /**
+   * タブを折りたたみます。
+   */
+  onClickCloseBtn () {
+    this.ui.collapse.collapse( 'toggle' );
+  }
+
+  /**
+   * enterの入力を監視します
+   * @param {event} e
+   */
+  onKeyupNameInput ( e ) {
+    if (e.which == 13) {
+      this.ui.nameInput.blur();
+    }
+  }
+
+  /**
+   * バッジの数字だけ更新
+   */
+  badgeUpdate () {
+    this.ui.badge.text( this.collection.length );
+  }
+
+  /**
+   * 並び替え機能を有効にします。
+   */
+  sortStart () {
+    this.ui.childViewContainer.sortable( {
+      forcePlaceholderSize: true,
+      handle              : '.js-variable-row-handle'
+    } ).bind( 'sortupdate', ()=> {
+      //console.log("variablesSort");
+      this.updateItemSortIndex( this.ui.childViewContainer.find( '.js-variable-row-model-id' ) );
+    } );
+  }
+
+  /**
+   * 並び替え機能を無効にします。
+   */
+  sortDestroy () {
+    if (this.ui.childViewContainer.sortable) {
+      this.ui.childViewContainer.sortable( 'destroy' );
+    }
+  }
+
+  /**
+   * 名前部分を再描画
+   */
+  redrawName () {
     this.ui.name.html( _.escape( this.model.get( 'name' ) ) );
     this.ui.name.removeClass( 'hide' );
     this.ui.nameInput.addClass( 'hide' );
-  },
-  onClickDelBtn      : function () {
-    this.model.destroy();
-  },
-  changeNameInput    : function () {
+  }
+
+  /**
+   * inputの中身をデータに反映して名前部分を再描画
+   */
+  changeNameInput () {
     if (this.model.save( 'name', this.ui.nameInput.val(), {silent: false} )) {
       // saveが実行されなければ…(変更が無い、もしくはerror)
       this.redrawName();
     }
-  },
-  onClickAddBtn      : function () {
-    this.collection.add( {}, {wait: true} );
-    this.ui.collapse.collapse( 'show' );
-    this.onRender();
-  },
+  }
+
   /**
-   *
-   * @param elements data-model-idの指定があるエレメントの配列
+   * 並び替えの結果をデータに反映します。
+   * @param {node[]} elements data-model-idの指定があるエレメントの配列
    */
-  updateItemSortIndex: function ( elements ) {
-    var _self = this;
-    _.each( elements, function ( element, index ) {
-      //noinspection JSUnresolvedVariable
-      _self.collection.get( element.dataset.modelId ).set( 'sort', index, {silent: true} );
+  updateItemSortIndex ( elements ) {
+    _.each( elements, ( element, index )=> {
+      this.collection.get( element.dataset.modelId ).set( 'sort', index, {silent: true} );
     } );
     this.model.updateList();
   }
-
-} );
-
-module.exports = ViewsDirPickerSettingVariable;
+}
